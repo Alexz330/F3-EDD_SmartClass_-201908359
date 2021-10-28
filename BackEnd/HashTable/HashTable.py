ef04c6a168e8a9d3fd17e682ee2026b5bc628f91
Alexis Lopez <alexislc330@gmail.com>
2021-10-28 10:23:12 -0600

    method getPorcentage agregada

--- BackEnd/HashTable/HashTable.py
+++ BackEnd/HashTable/HashTable.py
@@ -1,22 +1,47 @@
-# from re import T
-# from main import postearCursosEstudiante
+class apunte:
+    titulo:str
+    contenido:str
+
+class nodoHash:
+    carnet:int
+    apuntes:list = [apunte]
 
 
-# class HashTable:
-#     def __init__(self):
-#         self.table = [NO]
-#     def add(self):
-#         pass
-#     def get(self):
-#         pass
 
-#     def delete(self):
-#         pass
-#     def update(self):
-#         pass
-#     def functionHash(self,key,length):
-#         return key%length
-#     def getPorcentage(self):
+class HashTable:
+    def __init__(self):
+        self.tableLen = 7 
+        self.table = [None,None,None,None,None,None,None]
+        
+
+
+    def add(self,key,value):
+        position = self.functionHash(key,self.tableLen)
+        if(self.getPorcentageUse(self.tableLen) <= 0.5):
+            if self.table[position] == None:
+                
+
+    def get(self,key):
+        pass
+
+    def delete(self):
+        pass
+    def update(self):
+        pass
+    def functionHash(self,key,length):
+        return key%length
+    def getPorcentageUse(self,length):
+        counter = 0 
+        for i in self.table:
+            if(i is not None):
+                counter+=1
+
+        return counter/length
+
+apuntes =  HashTable()
+
+print()
+
 
 
 
