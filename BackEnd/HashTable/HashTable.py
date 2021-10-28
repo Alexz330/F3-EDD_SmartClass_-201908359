a192d251b471067d0bd08708af1e5bfdb4cc57b6
Alexis <alexislc330@gmail.com>
2021-10-27 23:52:34 -0600

    Creando tabla hash

--- BackEnd/HashTable/HashTable.py
+++ BackEnd/HashTable/HashTable.py
@@ -1,32 +1,22 @@
-import array
+# from re import T
+# from main import postearCursosEstudiante
+
 
 # class HashTable:
-#     def __init__(self,size):
-#         self.data  = array(size)
-    
-#     def hashMethod(self, key):
-#         hash =  0
-#         for i in key:
-#              hash = (hash + ord(i)*i ) % len(self.data)
-#         return hash
-    
-#     def set(self,key,value):
-#         address  = self.hashMethod(key)
+#     def __init__(self):
+#         self.table = [NO]
+#     def add(self):
+#         pass
+#     def get(self):
+#         pass
 
-#         if (self.data[address] is None):
-#             self.data[address] = []
-#         self.data[address].push([key, value])
-#         return self.data
-    
+#     def delete(self):
+#         pass
+#     def update(self):
+#         pass
+#     def functionHash(self,key,length):
+#         return key%length
+#     def getPorcentage(self):
 
-    
 
-# myHashTable = HashTable(50)
-
-# myHashTable.set("Alexis",2001)
-
-# for i in myHashTable.data:
-#     print(i)
-
-ccode = array.array.typecodes()
 
